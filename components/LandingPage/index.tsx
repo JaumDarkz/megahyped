393062a5110a6271c3dcb1891e54a19568c14e5c
João Vinícius <viniciuspkfr@gmail.com>
2023-03-09 14:44:32 -0300

    feat: add first commit

---
+++ components/LandingPage/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const LandingPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Navbar
