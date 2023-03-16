80c760e6b684f104ee1be157eeca9700f1b34e41
João Vinícius <viniciuspkfr@gmail.com>
2023-03-16 06:03:14 -0300

    feat: ref, fixes and social media links

--- components/LandingPage/components/Footer/index.tsx
+++ components/LandingPage/components/Footer/index.tsx
@@ -6,6 +6,7 @@ import opensea from './assets/opensea.svg'
 import telegram from './assets/telegram.svg'
 import discord from './assets/discord.svg'
 import twitter from './assets/twitter.svg'
+import instagram from './assets/instagram.svg'
 
 const Footer = () => {
   return (
@@ -17,11 +18,11 @@ const Footer = () => {
 
         <div className={styles.socialContainer}>
           <div className={styles.icon}>
-            <Image src={opensea} alt='Opensea' style={{cursor: 'pointer'}} onClick={() => window.open('https://opensea.io')} />
+            <Image src={opensea} alt='Opensea' style={{cursor: 'pointer'}} onClick={() => window.open('https://opensea.io/megahyped')} />
           </div>
 
           <div className={styles.icon}>
-            <Image src={telegram} alt='Telegram' style={{cursor: 'pointer'}} onClick={() => window.open('https://telegram.org')} />
+            <Image src={telegram} alt='Telegram' style={{cursor: 'pointer'}} onClick={() => window.open('https://telegram.org/megahypednft')} />
           </div>
 
           <div className={styles.icon}>
@@ -29,7 +30,11 @@ const Footer = () => {
           </div>
 
           <div className={styles.icon}>
-            <Image src={twitter} alt='Twitter' style={{cursor: 'pointer'}} onClick={() => window.open('https://twitter.com/megahypedNFT')} />
+            <Image src={twitter} alt='Twitter' style={{cursor: 'pointer'}} onClick={() => window.open('https://twitter.com/megahypednft')} />
+          </div>
+
+          <div className={styles.icon}>
+            <Image src={instagram} alt='Instagram' style={{cursor: 'pointer'}} onClick={() => window.open('https://instagram.com/megahypednft')} />
           </div>
         </div>
 
