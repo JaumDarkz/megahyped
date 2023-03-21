1e9df49d0977fa04d7c9f1d21f6989e6f6c0ff55
João Vinícius <viniciuspkfr@gmail.com>
2023-03-21 18:22:27 -0300

    feat: add FAQ section

--- components/LandingPage/components/FAQ/index.tsx
+++ components/LandingPage/components/FAQ/index.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
+import FAQCard from './components/FAQCard'
+
 import styles from './styles.module.scss'
 
 const FAQ = () => {
-  const [isOpen1, setIsOpen1] = useState(false)
-  const [isOpen2, setIsOpen2] = useState(false)
-  const [isOpen3, setIsOpen3] = useState(false)
-  const [isOpen4, setIsOpen4] = useState(false)
-  const [isOpen5, setIsOpen5] = useState(false)
-  const [isOpen6, setIsOpen6] = useState(false)
-
   return (
     <div style={{ background: '#303030' }}>
       <div className={styles.container}>
@@ -23,65 +17,11 @@ const FAQ = () => {
         </div>    
 
         <div className={styles.faqContainer}>
-          <div className={styles.faq} onClick={() => setIsOpen1(!isOpen1)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen1 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti?
-            </div>
-          : null}
-
-          <div className={styles.faq} onClick={() => setIsOpen2(!isOpen2)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen2 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti?
-            </div>
-          : null}
-
-          <div className={styles.faq} onClick={() => setIsOpen3(!isOpen3)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen3 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti?
-            </div>
-          : null}
-
-          <div className={styles.faq} onClick={() => setIsOpen4(!isOpen4)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen4 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti? 
-            </div>
-          : null}
-
-          <div className={styles.faq} onClick={() => setIsOpen5(!isOpen5)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen5 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti?
-            </div>
-          : null}
-
-          <div className={styles.faq} onClick={() => setIsOpen6(!isOpen6)}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem adipisci deserunt ea soluta minus ipsum id iure saepe,
-          </div>
-
-          {isOpen6 == true ? 
-            <div className={styles.openFaq}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, harum? Quam itaque tempore impedit vero voluptatibus, inventore dignissimos exercitationem ea nostrum, necessitatibus, dolorum architecto eveniet cumque veniam minima ratione corrupti?
-            </div>
-          : null}
+          <FAQCard question='Question here' answer='Answer here' />
+          <FAQCard question='Question here' answer='Answer here' />
+          <FAQCard question='Question here' answer='Answer here' />
+          <FAQCard question='Question here' answer='Answer here' />
+          <FAQCard question='Question here' answer='Answer here' />
         </div>
       </div>
     </div>
